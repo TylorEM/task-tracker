f529eecae581ad34ca162e8e62ef9297339490ad
TylorEM <TylorEM@users.noreply.github.com>
2021-08-22 15:00:28 -0400

    most recent

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,21 +2,21 @@
 // "FaTimes" is the Font Awesome "X" symbol
 // Don't forget to put "/fa" at the end of the import for Font Awesome
 
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes } from 'react-icons/fa';
 
-const Task = ({ task, onDelete }) => {
+const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className='task'>
+    <div onDoubleClick={onToggle} className="task">
       <h3>
         {task.text}
         <FaTimes
-          onClick={onDelete}
           style={{ color: 'red', cursor: 'pointer' }}
+          onClick={() => onDelete(task.id)}
         />
       </h3>
       <p>{task.day}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
