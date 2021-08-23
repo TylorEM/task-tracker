4d09ab9b03d07700591f2bce56f6a31a3c0013e7
TylorEM <TylorEM@users.noreply.github.com>
2021-08-23 11:01:45 -0400

    add state to inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import { useState } from 'react';
 
 const App = () => {
@@ -24,19 +25,27 @@ const App = () => {
       reminder: false,
     },
   ]);
-
+  // Add Task
+  const addTask = (task) => {
+    console.log(task);
+  };
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
   //Toggle Reminder
   const toggleReminder = (id) => {
-    console.log(id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
 
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onToggle={toggleReminder} onDelete={deleteTask} />
       ) : (
